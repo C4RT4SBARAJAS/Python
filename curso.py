2d51cc6a6467d345474bd26c562b3fae1bb17298
Heriberto <heribertocartas@gmail.com>
2021-07-10 14:39:45 -0500

    Agregué variables u objetos en pyton

--- curso.py
+++ curso.py
@@ -22,3 +22,30 @@
 
 # Raíz cuadrada
 25 ** 0.5
+
+# Variables u objetvos en Python
+# Debemos pensar en las variables como una caja donde guardamso cosas. 
+# Primero debemos darle un nombre al objeto o variable
+# Despues debemos agregar el asignador (en Python de usa el signo =)
+# Por último agregamos las cosas que queremos guardar en la variable.
+# Esto ser vería algo así: 
+# nombre_variable = cosas a guardar
+
+# Objetos númericos (integers)
+
+var1_num = 5
+
+# Objetos decimales
+
+var2_num = 3.5
+
+# Objetos de caracteres (strings)
+
+var1_str = "Heriberto"
+var2_str = 'Cartas'
+var3_str = "Barrera"
+
+# Objetos booleanos
+
+var1_boo = True
+var2_boo = False
