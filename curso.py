9f53187e4cb4e52a488848c249754347e16adeb0
Heriberto <heribertocartas@gmail.com>
2021-07-10 14:24:21 -0500

    Este es mi primer commit agregué operaciones aritméticas básicas

---
+++ curso.py
@@ -0,0 +1,24 @@
+# Bienvenido al curso de Python desde cero
+
+# Operaciones artiméticas básicas en Python (siguiendo el orden de operaciones PEMDAS):
+
+# Suma
+5 + 5
+
+# Resta
+10 - 2
+
+# División
+25 / 5
+
+# Cociente de una división
+25 // 5
+
+# Residuos de una división
+25 % 5
+
+# Potencia
+3**2
+
+# Raíz cuadrada
+25 ** 0.5
