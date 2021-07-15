9d28cc3d7bc73846e9e960a09128ac815b7bb4de
Heriberto <heribertocartas@gmail.com>
2021-07-15 16:59:42 -0500

    Agregué programa7.py

---
+++ programa7.py
@@ -0,0 +1,6 @@
+# Nombre del programa: Potencias de 2.
+
+potencia = (input('Ingrese la potencia: '))
+potencia = float(potencia)
+potencia = round(potencia, ndigits = None)
+print('2 elevado a ' + str(potencia) + ' es igual a: ' + str(2**potencia))
