460cbefb2db4731d3ea680846f0432539e5cf02f
Heriberto <heribertocartas@gmail.com>
2021-07-15 18:05:44 -0500

    Modifique programa7.py agregando un bucle while

--- programa7.py
+++ programa7.py
@@ -1,6 +1,39 @@
 # Nombre del programa: Potencias de 2.
 
-potencia = (input('Ingrese la potencia: '))
-potencia = float(potencia)
-potencia = round(potencia, ndigits = None)
-print('2 elevado a ' + str(potencia) + ' es igual a: ' + str(2**potencia))
+# contador = 0
+# print('2 elevado a ' + str(contador) + ' es igual a: ' + str(2**contador))
+
+# contador = 1
+# print('2 elevado a ' + str(contador) + ' es igual a: ' + str(2**contador))
+
+# contador = 2
+# print('2 elevado a ' + str(contador) + ' es igual a: ' + str(2**contador))
+
+# contador = 3
+# print('2 elevado a ' + str(contador) + ' es igual a: ' + str(2**contador))
+
+# contador = 4
+# print('2 elevado a ' + str(contador) + ' es igual a: ' + str(2**contador))
+
+# contador = 5
+# print('2 elevado a ' + str(contador) + ' es igual a: ' + str(2**contador))
+
+# contador = (input('Ingrese la potencia: '))
+# contador = float(contador)
+# contador = round(contador, ndigits = None)
+# print('2 elevado a ' + str(contador) + ' es igual a: ' + str(2**contador))
+
+def run():
+    LIMITE = 1000000
+
+
+    contador = 0
+    potencia_2 = 2**contador
+    while potencia_2 < LIMITE:
+      print('2 elevado a ' + str(contador) + ' es igual a: ' + str(potencia_2))  
+      contador = contador + 1
+      potencia_2 = 2**contador
+
+
+if __name__ == '__main__':
+    run()
