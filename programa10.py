b7748222f16d9c24279f2c7a95608425a0712af6
Heriberto <heribertocartas@gmail.com>
2021-07-16 02:17:34 -0500

    Agregué programa10.py ciclo for, continue y break

---
+++ programa10.py
@@ -0,0 +1,23 @@
+# Nombre del programa: impresor de números pares.
+
+# Utilizando un ciclo for, continue y break.
+def run():
+    # for contador in range(1000):
+    #     if contador % 2 != 0:
+    #         continue
+    #     print(contador)
+    
+    # for i in range(10000):
+    #     print(i)
+    #     if i == 5353:
+    #         break
+
+    texto = input('Escribe un texto: ')
+    for letra in texto:
+        if letra == 'o':
+            break
+        print(letra)
+
+
+if __name__ == '__main__':
+    run()
